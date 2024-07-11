eb279a819cff810966254b26ab68c424f954983e
k65860 <jy_0327@likelion.org>
2024-07-11 17:04:54 +0900

    Feat: User API communityId 추가

--- src/components/Mypage&Userpage/ListContainer.tsx
+++ src/components/Mypage&Userpage/ListContainer.tsx
@@ -1,26 +1,33 @@
 import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const mock = new MockAdapter(axios, { delayResponse: 500 });
+
+mock.onGet('/api/user').reply(200, {
+  communityId: [
+    { id: '1', category: 0, community: '안녕하세요', date: '2024-01-01' },
+    { id: '2', category: 0, community: '글입니다히히히', date: '2024-01-02' },
+    { id: '3', category: 1, community: '가운데정렬왜안돼', date: '2024-01-03' },
+  ],
+});
 
 const Container = styled.div`
-  margin: 30px 0 30px 0;
+  margin: 30px 0;
 `;
 
 const ContentList = styled.span`
-  padding: 10px 0 10px 0;
+  padding: 10px 0;
   text-align: center;
 `;
 
 const Title = styled.div`
   font-weight: bold;
-  font-size: var(--font-size-md-1); //16
+  font-size: 16px;
   text-align: center;
-  padding: 10px 0 10px 0;
+  padding: 10px 0;
 `;
-
-// const ListContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   margin: auto 0;
-// `;
 
 const DataContainer = styled.div`
   display: grid;
@@ -34,12 +41,26 @@ const GroupContent = styled(ContentList)`
   color: #6d987a;
 `;
 
-const ListContainer = () => {
-  const posts = [
-    { id: '1', group: '그룹1', title: '안녕하세요', date: '2024-01-01' },
-    { id: '2', group: '그룹2', title: '글입니다히히히', date: '2024-01-02' },
-    { id: '3', group: '그룹3', title: '가운데정렬왜안돼', date: '2024-01-03' },
-  ];
+interface CommunityPost {
+  id: string;
+  category: number;
+  community: string;
+  date: string;
+}
+
+const ListContainer: React.FC = () => {
+  const [posts, setPosts] = useState<CommunityPost[]>([]);
+
+  useEffect(() => {
+    axios.get('/api/user')
+      .then((response) => {
+        setPosts(response.data.communityId || []);
+        console.log(setPosts)
+      })
+      .catch((error) => {
+        console.error('Error fetching user data:', error);
+      });
+  }, []);
 
   return (
     <Container>
@@ -50,8 +71,8 @@ const ListContainer = () => {
       </DataContainer>
       {posts.map((post) => (
         <DataContainer key={post.id}>
-          <GroupContent>[{post.group}]</GroupContent>
-          <ContentList>{post.title}</ContentList>
+          <GroupContent>[{post.category === 0 ? '강아지' : '고양이'}]</GroupContent>
+          <ContentList>{post.community}</ContentList>
           <ContentList>{post.date}</ContentList>
         </DataContainer>
       ))}
