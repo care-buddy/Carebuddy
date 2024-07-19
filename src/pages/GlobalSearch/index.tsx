d827fbb949ab5e46028752d4fbeb58520794fb93
chacha <yushin.pak@gmail.com>
2024-07-20 08:45:36 +0900

    Feat: globalSearch 페이지 검색, select 로직 구현

--- src/pages/GlobalSearch/index.tsx
+++ src/pages/GlobalSearch/index.tsx
@@ -31,7 +31,10 @@ mock.onGet('/api/posts').reply(200, dummyPosts);
 const GlobalSearch: React.FC = () => {
   const [isWriteModalOpen, setIsWriteModalOpen] = useState(false); // 글 작성 모달
   const [posts, setPosts] = useState<PostData[] | null>(null); // 게시글 목록
-  // const [selectedPosts, setSelectedPosts] = useState<PostData[] | null>(null) // select로 선택된 게시글 목록
+  const [selectedPosts, setSelectedPosts] = useState<PostData[] | null>(null); // select로 선택된 게시글 목록
+  const [category, setCategory] = useState<number | string>('category');
+  const [community, setCommunity] = useState<string>('community');
+
   const [filteredPosts, setFilteredPosts] = useState<PostData[] | null>(null); // 검색된 게시글 목록
   const [searchTerm, setSearchTerm] = useState<string>(''); // 검색어
   const [isSearching, setIsSearching] = useState<boolean>(false); // 검색중인 상태
@@ -41,15 +44,19 @@ const GlobalSearch: React.FC = () => {
     setIsWriteModalOpen(false);
   };
 
-  // select(추후에 컴포넌트화)
-  const SelectOptions = [
+  // select option
+  const SelectCategoryOptions = [
+    { value: 'category', label: '종' },
     { value: 'dog', label: '강아지' },
     { value: 'cat', label: '고양이' },
   ];
 
-  const SelectGroupOptions = [
-    { value: 'group', label: '그룹' },
-    { value: 'eyes', label: '눈 / 피부 / 귀' },
+  // posts에서 options 추출하는 로직 설정해야함
+  const SelectCommunityOptions = [
+    { value: 'community', label: '커뮤니티' },
+    { value: '눈 / 피부 / 귀', label: '눈 / 피부 / 귀' },
+    { value: '코', label: '코' },
+    { value: '뇌·신경', label: '뇌·신경' },
   ];
 
   // 게시글 조회 API
@@ -89,6 +96,58 @@ const GlobalSearch: React.FC = () => {
     setFilteredPosts(filteredPost);
   }, [searchTerm, posts]);
 
+  // 카테고리 분류 선택
+  const handleCategoryOptions = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    if (event.target.value === 'dog') {
+      setCategory(0);
+    } else if (event.target.value === 'cat') {
+      setCategory(1);
+    } else {
+      setCategory('category');
+    }
+  };
+
+  // 커뮤니티 분류 선택
+  const handleCommunityOptions = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setCommunity(event.target.value);
+  };
+
+  // select 로직
+  useEffect(() => {
+    if (posts !== null) {
+      // 둘 다 선택한 경우
+      if (category !== 'category' && community !== 'community') {
+        const filteredPost = posts.filter(
+          (post) =>
+            post.communityId.category === category &&
+            post.communityId.community === community
+        );
+        setSelectedPosts(filteredPost);
+      } else if (category !== 'category' && community === 'community') {
+        // 카테고리만 선택
+        const filteredPost = posts.filter(
+          (post) => post.communityId.category === category
+        );
+        setSelectedPosts(filteredPost);
+      } else if (category === 'category' && community !== 'community') {
+        // 커뮤니티만 선택
+        const filteredPost = posts.filter(
+          (post) => post.communityId.community === community
+        );
+        setSelectedPosts(filteredPost);
+      } else {
+        // 둘 다 선택하지 않은 경우
+        setSelectedPosts(posts);
+      }
+    } else {
+      setSelectedPosts(null);
+    }
+  }, [posts, category, community]);
+
   return (
     <>
       <TopBar category="전체 검색" title="검색키워드" />
@@ -110,9 +169,14 @@ const GlobalSearch: React.FC = () => {
               <Select
                 selectStyle="round"
                 selectSize="sm"
-                options={SelectOptions}
+                options={SelectCategoryOptions}
+                onChange={handleCategoryOptions}
               />
-              <Select selectStyle="round" options={SelectGroupOptions} />
+              <Select
+                selectStyle="round"
+                options={SelectCommunityOptions}
+                onChange={handleCommunityOptions}
+              />
             </SelectContainer>
             <WriteButton setIsWriteModalOpen={setIsWriteModalOpen} />
             {isWriteModalOpen && (
@@ -125,7 +189,7 @@ const GlobalSearch: React.FC = () => {
             )}
           </FeedOptionContainer>
           <FeedBoxContainer>
-            {isSearching
+            {/* {isSearching
               ? filteredPosts?.map((post) => (
                   <FeedBox
                     key={post._id}
@@ -136,7 +200,9 @@ const GlobalSearch: React.FC = () => {
                     nickname={post.userId.nickName}
                     profileSrc={post.userId.profileImage[0]}
                     communityName={post.communityId.community}
-                    communityCategory={post.communityId.category === 0 ? '강아지' : '고양이'}
+                    communityCategory={
+                      post.communityId.category === 0 ? '강아지' : '고양이'
+                    }
                   />
                 ))
               : posts?.map((post) => (
@@ -149,7 +215,40 @@ const GlobalSearch: React.FC = () => {
                     nickname={post.userId.nickName}
                     profileSrc={post.userId.profileImage[0]}
                     communityName={post.communityId.community}
-                    communityCategory={post.communityId.category === 0 ? '강아지' : '고양이'}
+                    communityCategory={
+                      post.communityId.category === 0 ? '강아지' : '고양이'
+                    }
+                  />
+                ))} */}
+            {isSearching
+              ? selectedPosts?.map((post) => (
+                  <FeedBox
+                    key={post._id}
+                    postId={post._id}
+                    title={post.title}
+                    content={post.content}
+                    uploadedDate={formatDate(post.createdAt)}
+                    nickname={post.userId.nickName}
+                    profileSrc={post.userId.profileImage[0]}
+                    communityName={post.communityId.community}
+                    communityCategory={
+                      post.communityId.category === 0 ? '강아지' : '고양이'
+                    }
+                  />
+                ))
+              : selectedPosts?.map((post) => (
+                  <FeedBox
+                    key={post._id}
+                    postId={post._id}
+                    title={post.title}
+                    content={post.content}
+                    uploadedDate={formatDate(post.createdAt)}
+                    nickname={post.userId.nickName}
+                    profileSrc={post.userId.profileImage[0]}
+                    communityName={post.communityId.community}
+                    communityCategory={
+                      post.communityId.category === 0 ? '강아지' : '고양이'
+                    }
                   />
                 ))}
           </FeedBoxContainer>
