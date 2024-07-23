afe85bbc44ee7ca14b16791dc4fcc9b2d77d73c2
petitesize <petitesize22@gmail.com>
2024-07-23 13:57:48 +0900

    Feat: Date Util 함수 수정

--- src/utils/formatDate.tsx
+++ src/utils/formatDate.tsx
@@ -1,16 +1,17 @@
 // 날짜 formatting 함수
-interface DateProps {
-  rowDate: string;
-}
-
-const formatDate = ({ rowDate }: DateProps, slice: string) => {
+const formatDate = (rowDate: string) => {
   const date = new Date(rowDate);
+
+  // 날짜 객체 유효성 검사
+  if (Number.isNaN(date.getTime())) {
+    return 'Invalid Date';
+  }
 
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
 
-  return `${year}${slice}${month}${slice}${day}`;
+  return `${year}-${month}-${day}`;
 };
 
 export default formatDate;
