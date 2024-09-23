5a7046430103ebc597513b75324d1e65692c23b0
k65860 <jy_0327@likelion.org>
2024-09-23 12:48:04 +0900

    Feat: userpage api 연결

--- src/pages/Userpage/index.tsx
+++ src/pages/Userpage/index.tsx
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import axiosInstance from '@/utils/axiosInstance';
 import defaultImg from '@/assets/person.png';
 import ListContainer from '@/components/Mypage&Userpage/ListContainer';
 import PetCardContainer from '@/components/Mypage&Userpage/PetCardContainer';
 import TopBar from '@/components/common/TopBar';
-// 사진 데이터
+
 import { tempProfileSrc } from '@constants/tempData';
 import DefaultPetProfileImg from '@assets/defaultPetProfile.png';
 
@@ -110,22 +110,32 @@ const ImageBox = styled.div`
 
 interface UserData {
   email: string;
-  nickname: string;
-  introduction: string;
+  nickName: string;
+  introduce: string;
+  profileImage: string[];
   communityId: CommunityPost[];
   postId: PostId[];
-  buddyId: [];
-}
-
-interface CommunityPost {
-  id: string;
-  category: number;
-  community: string;
-  createdAt: string;
 }
 
 interface PostId {
   title: string;
+  content: string;
+  createdAt: Date;
+}
+
+interface ApiResponse {
+  email: string;
+  nickName: string;
+  introduce: string;
+  profileImage: string[];
+  communityId: CommunityPost[];
+  postId: ApiPostId[];
+}
+
+interface ApiPostId {
+  title: string;
+  content: string;
+  createdAt: string;
 }
 
 const ProfileContainer: React.FC<{ userData: UserData }> = ({ userData }) => (
@@ -139,12 +149,12 @@ const ProfileContainer: React.FC<{ userData: UserData }> = ({ userData }) => (
       <Info>
         <InputList>
           <List>
-            <ListItem bold>{userData.nickname}</ListItem>
+            <ListItem bold>{userData.nickName}</ListItem>
           </List>
         </InputList>
         <InputList>
           <List>
-            <ListItem>{userData.introduction}</ListItem>
+            <ListItem>{userData.introduce}</ListItem>
           </List>
         </InputList>
       </Info>
@@ -153,54 +163,16 @@ const ProfileContainer: React.FC<{ userData: UserData }> = ({ userData }) => (
 );
 
 const Userpage: React.FC = () => {
-  // user api Mock 설정
-  const mock = new MockAdapter(axios, { delayResponse: 500 });
-
-  mock.onGet('/api/user').reply(200, {
-    email: 'carebuddy@naver.com',
-    nickname: '케어버디',
-    introduction: '소개글입니다^^',
-    communityId: [
-      // id는 api 고유 id값이 아니라 map순환을 위해 임시 부여한 id를 의미함
-      { id: '1', category: 0, community: '눈', createdAt: '2024-01-01' },
-      { id: '2', category: 0, community: '위식도', createdAt: '2024-01-02' },
-      { id: '3', category: 1, community: '중성화', createdAt: '2024-01-03' },
-    ],
-    postId: [
-      { title: '안녕하세요' },
-      { title: '글제목입니다 ㅎㅎ' },
-      { title: '동물이 최고야!!' },
-    ],
-    // buddy profile 용 임시로 추가했습니다: 지영
-    buddyId: [
-      {
-        buddyImage: DefaultPetProfileImg,
-        name: '후이',
-        species: 0,
-        kind: '샴',
-        age: 1,
-        weight: 2,
-        deletedAt: null,
-      },
-      {
-        buddyImage: tempProfileSrc,
-        name: '쿠키',
-        species: 0,
-        kind: '말티즈',
-        age: 3,
-        weight: 13,
-        deletedAt: null,
-      },
-    ],
-  });
   const [userData, setUserData] = useState<UserData>({
     email: '',
-    nickname: '',
-    introduction: '',
+    nickName: '',
+    introduce: '',
+    profileImage: [],
     communityId: [],
     postId: [],
-    buddyId: [],
   });
+
+  const userId = '66b9b34ae9a13c88c643e361'; // userId 지정 -> 추후 변환 예정
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -208,17 +180,35 @@ const Userpage: React.FC = () => {
     const fetchData = async () => {
       setIsLoading(true);
       try {
-        const response = await axios.get('/api/user');
-        setUserData(response.data);
+        const response = await axiosInstance.get<{ message: ApiResponse }>(`users/${userId}`);
+        const data = response.data.message;
+
+        const mappedData: UserData = {
+          email: data.email || '',
+          nickName: data.nickName || '',
+          introduce: data.introduce || '',
+          profileImage: data.profileImage || [],
+          communityId: data.communityId || [],
+          postId: data.postId
+            ? data.postId.map((post) => ({
+              title: post.title,
+              content: post.content,
+              createdAt: new Date(post.createdAt),
+            }))
+            : [],
+        };
+        console.log('user 데이터: ', data)
+        setUserData(mappedData);
+
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error('사용자 데이터 가져오기 오류:', error);
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchData();
-  }, []);
+  }, [userId]);
 
   const contentItems = [
     {
@@ -236,7 +226,6 @@ const Userpage: React.FC = () => {
       content: '작성 글 목록',
       component: (
         <ListContainer
-          communityPosts={userData.communityId}
           postIds={userData.postId}
           isLoading={isLoading}
         />
