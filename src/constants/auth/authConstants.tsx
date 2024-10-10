4cc9f286a4dffccfb6a43c259fec0ed68a5f7faa
petitesize <petitesize22@gmail.com>
2024-10-10 14:04:15 +0900

    Fix: 토큰 만료 시간 수정

--- src/constants/auth/authConstants.tsx
+++ src/constants/auth/authConstants.tsx
@@ -1,4 +1,5 @@
 // const JWT_EXPIRY_TIME = 1 * 30 * 1000; // 테스트용 만료 시간
 const JWT_EXPIRY_TIME = 24 * 3600 * 1000; // 만료 시간
 
-export default { JWT_EXPIRY_TIME };
+// eslint-disable-next-line import/prefer-default-export
+export { JWT_EXPIRY_TIME };
