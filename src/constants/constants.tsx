76ee71987bb607a6ced9b8b8dcfc9e841f39c5be
chacha <yushin.pak@gmail.com>
2024-10-28 16:17:14 +0900

    https로 변경

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -2,5 +2,7 @@
 // export const API_URL = 'http://localhost:3003/api/';
 
 // 배포테스트용 상수
-export const API_URL = 'https://carebuddy.store:443/api/';
+export const API_URL = 'https://carebuddy.store/api/';
+// export const API_URL = 'http://carebuddy.store:443/api/';
+// export const API_URL = 'https://carebuddy.store:443/api/';
 // export const API_URL = 'https://34.64.48.225:3001/api/';
