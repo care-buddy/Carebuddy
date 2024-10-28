93597985db294d7e312dd04c928ce5c36f9aa5d3
chacha <yushin.pak@gmail.com>
2024-10-28 14:17:16 +0900

    Fix: 백엔드 주소 변경(http -> https)

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -2,4 +2,4 @@
 // export const API_URL = 'http://localhost:3003/api/';
 
 // 배포테스트용 상수
-export const API_URL = 'http://34.64.48.225:3001/api/';
+export const API_URL = 'https://34.64.48.225:3001/api/';
