780d6ae0e0693a4628db7278ca5b41e7891bbb8c
petitesize <petitesize22@gmail.com>
2024-10-28 10:41:39 +0900

    Fix: 카테고리 key 값 관련 오류 수정 (#85)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -159,6 +159,7 @@ const Home: React.FC = () => {
                 selectSize="sm"
                 options={categoryOptions}
                 onChange={handleCategoryOptions}
+                // key={categoryOptions.values}
               />
               <Select
                 selectStyle="round"
