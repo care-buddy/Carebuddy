bfd15194e2b1c67c945495b6da97ef950db14e8a
chacha <yushin.pak@gmail.com>
2024-10-29 11:27:30 +0900

    Fix: 상수 변경

--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -1,11 +1,8 @@
-/* eslint-disable import/prefer-default-export */
-export const API_URL = 'http://localhost:3001/api/';
 
-// 배포테스트용 상수
-// export const API_URL = 'https://carebuddy.store/api/';
-// export const API_URL = 'http://carebuddy.store:443/api/';
-// export const API_URL = 'https://carebuddy.store:443/api/';
-// export const API_URL = 'https://port-0-back-m2swdvfbbfb61876.sel4.cloudtype.app/api/';
+// 배포테스트&배포용 상수
+// export const API_URL = 'http://localhost:3001/api/';
+export const API_URL = 'https://port-0-back-m2swdvfbbfb61876.sel4.cloudtype.app/api/';
 
+// 홈페이지용 상수
 export const LOCAL_HOMEPAGE = 'http://localhost:5173';
 export const HOMEPAGE = 'https://carebuddy.vercel.app';
